052e463ed36902a4633c344fcf3f010e5ca4c14e
deepak91144 <53552887+deepak91144@users.noreply.github.com>
2020-09-29 01:37:48 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/brands.css" integrity="sha384-MiOGyNsVTeSVUjE9q/52dpdZjrr7yQAjVRUs23Bir5NhrTq0YA0rny4u/qe4dxNj" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/fontawesome.css" integrity="sha384-PRy/NDAXVTUcXlWA3voA+JO/UMtzWgsYuwMxjuu6DfFPgzJpciUiPwgsvp48fl3p" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="header">
+        <nav class="menu">
+            <ul>
+                <li> <a href="http://" target="_blank" rel="noopener noreferrer">about</a> </li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">contact</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">service</a></li>
+            </ul>
+        </nav>
+        <div class="logo">
+            <img src="pic-2.png" alt="">
+        </div>
+        <nav class="social-icon-nav">
+            <ul class="social-icon">
+                <li><i class="fab fa-facebook"></i></li>
+                <li><i class="fab fa-twitter"></i></li>
+                <li><i class="fab fa-whatsapp-square"></i></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- main content -->
+    <div class="main">
+        <aside class="side-bar">
+            <ul>
+                <li>link1</li>
+                <li>link2</li>
+                <li>link3</li>
+            </ul>
+        </aside>
+        <article class="article-content">
+            <div class="intro-text">
+                <h1>welcomes you</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <h2 class="our-service">our services</h2>
+            <div class="service-container">
+
+                <div class="service-box">
+                    <div class="service-img">
+                        <img src="course.png" alt="">
+                    </div>
+                    <h2 class="heading-text">web development</h2>
+                    <p class="service-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div class="service-box">
+                    <div class="service-img">
+                        <img src="about.png" alt="">
+                    </div>
+                    <h2 class="heading-text">mobile app</h2>
+                    <p class="service-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div class="service-box">
+                    <div class="service-img">
+                        <img src="contact.png" alt="">
+                    </div>
+                    <h2 class="heading-text">seo service</h2>
+                    <p class="service-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+
+        </article>
+    </div>
+    <footer>
+        <h3>all right reserved</h3>
+    </footer>
+</body>
+
+</html>
